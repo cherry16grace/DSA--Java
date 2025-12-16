95a020a93e1716cb30712fa45567bd588e678c68
cherry16grace <gracenciyacheril@gmail.com>
2025-12-16 18:41:56 +0530

    solved remove duplicates from sorted array

---
+++ removeDuplicates.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class removeDuplicates{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int k=1;
+        for(int j=1;j<n;j++){
+            if(arr[j]!=arr[j-1]){
+                arr[k++]=arr[j];
+            }
+        }
+        System.out.print(k);
+    }
+}
