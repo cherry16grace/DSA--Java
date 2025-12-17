c7880a7649d6ba0fc2cfb63aa14983e6acfb3036
cherry16grace <gracenciyacheril@gmail.com>
2025-12-17 19:54:26 +0530

    solved house robber problem

---
+++ houseRobber.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class houseRobber{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int[] dp=new int[n+1];
+        dp[0]=arr[0];
+        dp[1]=Math.max(arr[0],arr[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(dp[i-1],dp[i-2]+arr[i]);
+        }
+        System.out.print(dp[n-1]);
+
+    }
+
+}
